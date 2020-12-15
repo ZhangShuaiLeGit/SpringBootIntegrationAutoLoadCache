5486a3ec504771c06924236707a2b42336e3279e
张帅6 <zhangshuai6@le.com>
2020-12-15 17:42:12 +0800

    服务注册

--- spring/src/main/java/com/letv/spring/SpringBootStarterApp.java
+++ spring/src/main/java/com/letv/spring/SpringBootStarterApp.java
@@ -2,14 +2,17 @@ package com.letv.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringBootStarterApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarterApp.class, args);
     }
-
 }
--- spring/src/main/java/com/letv/spring/controller/BeanFactory.java
+++ spring/src/main/java/com/letv/spring/controller/BeanFactory.java
@@ -1,7 +1,13 @@
 package com.letv.spring.controller;
 
 import com.letv.spring.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,8 +26,15 @@ public class BeanFactory {
         return "hello,boot";
     }
 
-    @RequestMapping("/test2")
-    public Object sayHello2(){
-        return "boot热部署";
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String Index() {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        log.info("/hello, host:{}, serverId:{}", instance.getHost(), instance.getServiceId());
+        return "hello client";
     }
 }
