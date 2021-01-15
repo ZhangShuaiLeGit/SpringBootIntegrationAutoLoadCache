32131b7edbe1e90db0c14b739f811621e939025c
张帅6 <zhangshuai6@le.com>
2021-01-15 17:28:24 +0800

    sentinel使用Apollo存储

--- spring/src/main/java/com/letv/sentinel/SpringBootStarterApp.java
+++ spring/src/main/java/com/letv/sentinel/SpringBootStarterApp.java
@@ -6,6 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootStarterApp {
 
+    /**
+     * sentinel测试博客：https://blog.csdn.net/yuanchangliang/article/details/111663821
+     * resource：资源名，即限流规则的作用对象
+     * limitApp：流控针对的调用来源，若为 default 则不区分调用来源
+     * grade：限流阈值类型（QPS 或并发线程数）；0代表根据并发数量来限流，1代表根据QPS来进行流量控制
+     * count：限流阈值
+     * strategy：调用关系限流策略
+     * controlBehavior：流量控制效果（直接拒绝、Warm Up、匀速排队）
+     * clusterMode：是否为集群模式
+     */
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarterApp.class, args);
     }
--- spring/src/main/java/com/letv/sentinel/service/SentinelService.java
+++ spring/src/main/java/com/letv/sentinel/service/SentinelService.java
@@ -3,17 +3,18 @@ package com.letv.sentinel.service;
 import com.alibaba.csp.sentinel.EntryType;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.TimeoutException;
 
 @Service
 public class SentinelService {
     static int count = 1;
 
     @SentinelResource(value = "doSomeThing", entryType = EntryType.IN,
-            blockHandler = "exceptionHandler", fallback = "fallbackHandler", exceptionsToTrace = TimeoutException.class)
+            blockHandler = "exceptionHandler", fallback = "fallbackHandler")
     public String lazySay(String some) {
+        DegradeRule degradeRule = new DegradeRule();
+        System.out.println(degradeRule.toString());
         if (count % 3 == 0) {
             try {
                 throw new RuntimeException("歇歇吧");
@@ -26,12 +27,8 @@ public class SentinelService {
     }
 
     // 限流与阻塞处理
-    public String exceptionHandler(String some, BlockException ex) throws Exception {
-        try {
-            throw new TimeoutException(ex.getMessage());
-        } finally {
-            System.out.println(ex.getMessage());
-        }
+    public String exceptionHandler(String some, BlockException ex) {
+        return  "blockHandler：" + some + ex;
     }
 
     // 熔断处理
